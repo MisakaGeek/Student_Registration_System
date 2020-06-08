c05c3ac81f38e0fb24e6e08577765044ac8352c2
MisakaGeek <38345080+MisakaGeek@users.noreply.github.com>
2020-06-08 20:51:53 +0800

    Update ModifyStudent.java

--- Student_Registration_System/Student_Registration_System/src/Login/ModifyStudent.java
+++ Student_Registration_System/Student_Registration_System/src/Login/ModifyStudent.java
@@ -1,5 +1,5 @@
 package Login;
-import java.awt.*;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedInputStream;
@@ -8,205 +8,144 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.swing.*;
 
-
-import java.util.*;
-import javax.swing.*;
-public class MaintainStudent extends JFrame {
+public class ModifyStudent extends JFrame{
+	private  String sid;
 	private Socket socket;
 	private DataInputStream dis;
 	private DataOutputStream dos;
-	private Vector<String> idd=new Vector<String>();
-	public MaintainStudent(Socket socket)   {
-		super("学生信息维护");
+	public ModifyStudent(String da[],Socket socket) 
+	{
+		super("修改学生信息");
 		this.socket=socket;
 		try {
 			dis = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
 			dos = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
-		} catch (IOException e2) {
+		} catch (IOException e3) {
 			// TODO Auto-generated catch block
-			e2.printStackTrace();
+			e3.printStackTrace();
 		}
 
-     
+      
 		
-		JPanel con=new JPanel();
-		setBounds(150, 150, 1150, 500);//
-		//setLayout(null);
-		con.setLayout(null);
-		//con.setPreferredSize(new Dimension(1000,1000));
-		JScrollPane sp1 = new JScrollPane(con);
-		//sp1.setBounds(400,200,100,100);
-		//sp1.setLayout(null);
-		//this.getContentPane().add(sp1);
-		this.add(sp1);
-		JComboBox jc=new JComboBox();//选择查找标准的下拉框
-		jc.setEditable(false);//不可编辑
-	    jc.setEnabled(true);
-	    String[] arr ={"----查找标准----"};
-		ComboBoxModel cbm = new DefaultComboBoxModel(arr);//下拉框的显示不可选
-		jc.setModel(cbm);
-		jc.addItem("sid");//添加选项
-	    jc.addItem("姓名");
-	    jc.addItem("出生日期");
-	    jc.addItem("SSN");
-	    jc.addItem("职称");
-	    jc.addItem("年级");
-	   
-	    con.add(jc);
-		jc.setBounds(420, 50, 110, 24);
-		JButton jb=new JButton("查找");//查找按钮
-		con.add(jb);
-		jb.addActionListener(new ActionListener() {
-			//给查找按钮添加事件监听
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				
-				if(jc.getSelectedIndex()!=0)
-				{
-					SearchStudent sp;
-					sp = new SearchStudent(socket);
-					sp.searchType(jc.getSelectedItem().toString());
-					
-					dispose();
-				}
-				
-			}
-		});
-		jb.setBounds(540, 50, 60, 25);
-		JButton b=new JButton("添加");
-		con.add(b);
-		b.setBounds(610, 50, 60, 25);
-		JButton b2=new JButton("返回");
-		con.add(b2);
-		b2.setBounds(1015, 50, 60, 25);
-		JLabel jl1=new JLabel("序号");
-		JLabel jl3=new JLabel("sid");
-		JLabel jl4=new JLabel("密码");
-		JLabel jl5=new JLabel("姓名");
-		JLabel jl6=new JLabel("出生日期");
-		JLabel jl7=new JLabel("SSN");
-		JLabel jl8=new JLabel("年级");
-		//JLabel jl9=new JLabel("院系");
-		jl1.setBounds(35, 90, 60, 25);
-		jl3.setBounds(135, 90, 60, 25);
-		jl4.setBounds(235, 90, 60, 25);
-		jl5.setBounds(365, 90, 60, 25);	
-		jl6.setBounds(465, 90, 60, 25);
-		jl7.setBounds(565, 90, 60, 25);
-		jl8.setBounds(715, 90, 60, 25);	
-		//jl9.setBounds(815, 90, 60, 25);
-		con.add(jl3);
-		con.add(jl4);
-		con.add(jl5);
-		con.add(jl6);
-		con.add(jl7);
-		con.add(jl8);
-		String sql="select * from student";
-		String data="";
-		try {
-			dos.writeUTF("341"+"#"+sql);
-			dos.flush();
-			data=dis.readUTF();
-		} catch (IOException e2) {
-			// TODO Auto-generated catch block
-			e2.printStackTrace();
-		}
-		String da[];
-		da=data.split("#");
+		this.setBounds(500, 100, 500, 500);
+		JButton b1=new JButton("确定");
+		JButton b2=new JButton("取消");
+		JLabel jl1=new JLabel("sid");
+		JLabel jl2=new JLabel("登录密码");
+		JLabel jl3=new JLabel("姓名");
+		JLabel jl4=new JLabel("出生日期");
+		JLabel jl5=new JLabel("SSN");
+		JLabel jl6=new JLabel("年级");
+		//JLabel jl7=new JLabel("院系");
+		JTextField tf1=new JTextField();
+		JTextField tf2=new JTextField();
+		JTextField tf3=new JTextField();
+		JTextField tf4=new JTextField();
+		JTextField tf5=new JTextField();
+		JTextField tf6=new JTextField();
+		//JTextField tf7=new JTextField();
+		this.setLayout(null);
+		//this.add(jl1);
+		//this.add(jc);
+		this.add(jl1);
+		this.add(tf1);
+		this.add(jl2);
+		this.add(tf2);
+		this.add(jl3);
+		this.add(tf3);
+		this.add(jl4);
+		this.add(tf4);
+		this.add(jl5);
+		this.add(tf5);
+		this.add(jl6);
+		this.add(tf6);
+		//this.add(jl7);
+		//this.add(tf7);
+		this.add(b1);
+		this.add(b2);
+		//this.add(jl5);
+		jl1.setBounds(150, 50, 60, 30);
+		jl2.setBounds(150, 90, 60, 30);
+		jl3.setBounds(150, 130, 60, 30);
+		jl4.setBounds(150, 170, 60, 30);
+		jl5.setBounds(150, 210, 60, 30);
+		jl6.setBounds(150, 250, 60, 30);
+		//jl7.setBounds(150, 290, 60, 30);
+		tf1.setBounds(230, 50, 120, 25);
+		tf2.setBounds(230, 90, 120, 25);
+		tf3.setBounds(230, 130, 120, 25);
+		tf4.setBounds(230, 170, 120, 25);
+		tf5.setBounds(230, 210, 120, 25);
+		tf6.setBounds(230, 250, 120, 25);
+		//tf7.setBounds(230, 290, 120, 25);
+		b1.setBounds(180, 350, 60, 25);
+		b2.setBounds(250, 350, 60, 25);
+		
 		int i=0;//i是当前信息的序号，用来实现删除修改的定位
 		int j=0;//j是为了得到传回来的数据，因为是用#分开的数据	
        while(!da[j].equals(" ")){
-    	    final int k=i;
+    	   final int k=i;
            // 通过字段检索
-            String sid  = da[j++];
-       		String password=da[j++];
+            sid  = da[j++];
+       	    String password=da[j++];
             String name = da[j++];
             String birthday= da[j++];			  				    
-			String ssn= da[j++];
-			String status=da[j++];
-			JLabel jl10=new JLabel(i+"");
-			JLabel l1=new JLabel(sid);
-			JLabel l2=new JLabel(password);
-			JLabel l3=new JLabel(name);
-			JLabel l4=new JLabel(birthday);
-			JLabel l5=new JLabel(ssn);
-			JLabel l6=new JLabel(status);
-			//JLabel l7=new JLabel(depart);
-			con.add(jl1);
-			con.add(jl10);
-			con.add(l1);
-			con.add(l2);
-			con.add(l3);
-			con.add(l4);
-			con.add(l5);
-			con.add(l6);
-			//con.add(l7);
-			jl10.setBounds(35, 120+70*i, 60, 25);//控制位置随信息的条数增加变化
-			l1.setBounds(135, 120+70*i, 60, 25);
-			l2.setBounds(235, 120+70*i, 60, 25);	
-			l3.setBounds(365, 120+70*i, 60, 25);
-			l4.setBounds(465, 120+70*i, 60, 25);
-			l5.setBounds(565, 120+70*i, 60, 25);
-			l6.setBounds(715, 120+70*i, 60, 25);
-			//l7.setBounds(815, 120+70*i, 60, 25);
-			JButton jb1=new JButton("删除");//对此条信息删除
-			JButton jb2=new JButton("修改");//对此条信息修改
-			con.add(jb1);
-			jb1.setBounds(945, 120+70*i, 60, 25);
-			con.add(jb2);
-			jb2.setBounds(1015, 120+70*i, 60, 25);
-			idd.add(sid);
-			jb1.addActionListener(new ActionListener() {
-				//删除按钮添加事件监听
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					// TODO Auto-generated method stub
-					int n = JOptionPane.showConfirmDialog(null, "确定要删除吗?", "消息框", JOptionPane.YES_NO_OPTION);
-					if (n == JOptionPane.YES_OPTION) {
-						try {
-							Search_Del rg=new Search_Del(socket);
-							rg.delS(k,idd);
-						} catch (IOException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
-						//删除函数
-						dispose();
-					}
-				}
-			});
-			jb2.addActionListener(new ActionListener() {
-				//修改按钮添加事件监听
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					// TODO Auto-generated method stub
-					Search_Del rg;
-					try {
-						rg = new Search_Del(socket);
-						rg.modifyS(k,idd);
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-					//修改函数
-					dispose();
-					
-				}
-			});	
-			i++;
-          
-       } 
-       con.setPreferredSize(new Dimension(1100,150+70*i)); 		
-		b.addActionListener(new ActionListener() {	
-			//添加按钮添加事件监听
+		    String ssn= da[j++];
+		    String status=da[j++];
+		    tf1.setText(sid);
+			tf2.setText(name);
+			tf3.setText(password);
+			tf4.setText(birthday);
+			tf5.setText(ssn);
+			tf6.setText(status);				
+			}
+        
+	    b1.addActionListener(new ActionListener() {
+			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				AddStudent add=new AddStudent(socket);
-				dispose();
+				// TODO Auto-generated method stub
+				int n = JOptionPane.showConfirmDialog(null, "确定要修改吗?", "消息框", JOptionPane.YES_NO_OPTION);
+				if (n == JOptionPane.YES_OPTION) {
+					String sid1=tf1.getText().trim();
+					String password1= tf2.getText().trim();
+					String name1=tf3.getText().trim();
+					String birthday1= tf4.getText().trim();				  				    
+					String ssn1= tf5.getText().trim();
+					String status1= tf6.getText().trim();
+				//	String depart1= tf7.getText().trim();			
+						String sql="update Student set sid='"+sid1+"',password='"+password1+"',name='"+name1+"',birthday='"+birthday1+"',ssn='"+ssn1+"',status='"+status1+"' where sid='"+sid+"'";   //SQL语句
+						try {
+							dos.writeUTF("33"+"#"+sql);
+							dos.flush();
+							int success=dis.readInt();
+					         System.out.println(success);
+					         if(success==0)
+					         {
+					        	 JOptionPane.showMessageDialog(null, "操作失败");
+					        	 
+					         }
+					         else
+					         {	
+					        	 JOptionPane.showMessageDialog(null, "修改成功");
+					         }
+						} catch (IOException e2) {
+							// TODO Auto-generated catch block
+							e2.printStackTrace();
+						}			        
+				    
+				    dispose();
+				    MaintainStudent a=new MaintainStudent(socket);
+				    
+				}
 			}
 		});
 		b2.addActionListener(new ActionListener() {
@@ -215,12 +154,11 @@ public class MaintainStudent extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 				dispose();
-				Registrar_GUI rg=new Registrar_GUI(socket);
+				MaintainStudent a=new MaintainStudent(socket);
 			}
 		});
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setVisible(true);
+		this.setVisible(true);
+
 	}
-		
-	
 }
